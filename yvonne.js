b8d7f7334a3aa33ac99f87a62ba15673305b44d5
Yvonne Lange <yvonne-lange@web.de>
2021-03-19 14:10:28 +0100

    Finalized the toggle for answer button

--- yvonne.js
+++ yvonne.js
@@ -5,11 +5,11 @@ for (let i = 0; i < button.length; i++){
 button[i].addEventListener("click", () => {
  const shown = answer[i].classList.toggle("question-box__answer");
 if (shown) {
-answer[i].style.display = "block";
 
+    answer[i].style.display = "none";
 } else {
+    answer[i].style.display = "block";
 
-answer[i].style.display = "none";
 }
 });
 }
