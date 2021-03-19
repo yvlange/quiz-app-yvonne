85da9a49fd7d7181644ca814fadec898120b8de4
Yvonne Lange <yvonne-lange@web.de>
2021-03-19 12:56:58 +0100

    Toggled the answer button

---
+++ yvonne.js
@@ -0,0 +1,15 @@
+const button = document.querySelectorAll('button');
+const answer = document.querySelectorAll('.question-box__answer');
+
+for (let i = 0; i < button.length; i++){
+button[i].addEventListener("click", () => {
+ const shown = answer[i].classList.toggle("question-box__answer");
+if (shown) {
+answer[i].style.display = "block";
+
+} else {
+
+answer[i].style.display = "none";
+}
+});
+}
