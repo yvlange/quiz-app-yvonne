453d515945e0bb356a010bc91e707308ea02ca6f
Yvonne Lange <yvonne-lange@web.de>
2021-03-19 17:30:02 +0100

    JS on create page

--- yvonne.js
+++ yvonne.js
@@ -9,7 +9,26 @@ if (shown) {
     answer[i].style.display = "none";
 } else {
     answer[i].style.display = "block";
-
 }
 });
 }
+
+const bookmark = document.querySelectorAll(".question-box__bookmark");
+
+for (let i = 0; i < bookmark.length; i++) {
+   bookmark[i].addEventListener("click", () => {
+       
+if(bookmark[i].classList.contains("far")) {
+    bookmark[i].classList.remove("far", "fa-bookmark");
+       bookmark[i].classList.add("fas", "fa-bookmark"); 
+
+} else  {
+    bookmark[i].classList.remove("fas", "fa-bookmark");
+    bookmark[i].classList.add("far", "fa-bookmark"); 
+   
+}
+   });
+}
+
+
+
